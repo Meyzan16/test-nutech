e2daf24217765af3fd56d39f49178f3515fb6715
Meyzan16 <Meyzan1605@gmail.com>
2023-05-13 09:08:09 +0700

    remove img with index.js

--- pages/index.js
+++ pages/index.js
@@ -12,11 +12,10 @@ export default function Home() {
           </h2>
 
           <div className="md:flex bg-teal-500 rounded-lg items-center hidden overflow-hidden">
-            <img src={session?.user?.image} alt="" className="w-10 h-10 object-cover" />
-            <span className="text-base px-2 text-white">
-              {session?.user?.name}
-            </span>
-          </div>
+              <span className="text-base px-2 text-white">
+                {session?.user?.name}
+              </span>
+            </div>
         </div>
     </Layout>
   ) 

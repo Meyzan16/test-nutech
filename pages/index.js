ab9940288dffcd487e9c0fdb4688d9170254be48
Meyzan16 <Meyzan1605@gmail.com>
2023-05-14 21:29:30 +0700

    remove crenditilas with google

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,24 @@
 import Layout from "@/components/Layout";
-import { useSession } from "next-auth/react";
+// import { useSession } from "next-auth/react";
 
 export default function Home() {
-  const {data: session} = useSession();
-  console.log({session});
+  // const {data: session} = useSession();
+  // console.log({session});
   return (
     <Layout>
         <div className="text-teal-500 flex justify-between">
           <h2>
-            Hello, <b>{session?.user?.email}</b> 
+            Hello, 
+            
+            {/* <b>{session?.user?.email}</b>  */} 
           </h2>
-
-          <div className="md:flex bg-teal-500 rounded-lg items-center hidden overflow-hidden">
+        
+ 
+          {/* <div className="md:flex bg-teal-500 rounded-lg items-center hidden overflow-hidden">
               <span className="text-base px-2 text-white">
                 {session?.user?.name}
               </span>
-            </div>
+            </div>  */}
         </div>
     </Layout>
   ) 
